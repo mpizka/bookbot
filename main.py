8801c324b9af981df40de3f938a49be1ccf1ee69
Manfred Pizka <mpizka@icloud.com>
2023-10-29 10:36:52 +0100

    add chose file, errhandling, report footer

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 #! /usr/bin/env python
 """Text analysis and reporting script"""
 
+import sys
+
 
 def count_words(s: str) -> int:
     """Count words in s"""
@@ -30,16 +32,24 @@ def print_report(path: str, num_words: int, letters: dict) -> None:
     print()
     for char in sorted(letters.keys()):
         print(f"The '{char}' character was found {letters[char]} times")
+    print("--- End of report ---")
 
 
 def main():
     """Script main entry point"""
-    path = "books/frankenstein.txt"
-    with open(path) as f:
-        file_contents = f.read()
-        num_words = count_words(file_contents)
-        letters = count_letters(file_contents)
-        print_report(path, num_words, letters)
+    if len(sys.argv) != 2:
+        print("error: no file to analyse", file=sys.stderr)
+        sys.exit(1)
+    path = sys.argv[1]
+    try:
+        with open(path) as f:
+            file_contents = f.read()
+            num_words = count_words(file_contents)
+            letters = count_letters(file_contents)
+            print_report(path, num_words, letters)
+    except Exception as err:
+        print(f"error: {err}", file=sys.stderr)
+        sys.exit(1)
 
 
 main()
